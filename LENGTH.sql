c299750c1f92de19296872cd2408b8264e8cdf5a
Surajkumar Yadav <76464630+suraj4502@users.noreply.github.com>
2023-05-30 15:43:19 +0530

    using leght function along with where

---
+++ LENGTH.sql
@@ -0,0 +1,8 @@
+/*
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+
+SELECT CITY, LENGTH(CITY) AS smallest_city FROM STATION 
+ORDER BY smallest_city, CITY LIMIT 1; 
+SELECT CITY, LENGTH(CITY) AS longest_city FROM STATION 
+ORDER BY longest_city DESC LIMIT 1;
